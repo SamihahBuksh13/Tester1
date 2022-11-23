b897c558b753e87b281f732d9c826074fa579123
SamihahBuksh13 <77760661+SamihahBuksh13@users.noreply.github.com>
2022-11-24 10:39:12 +1300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+#get input from users
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+#add the two numbers
+total = num1 + num2 
+
+#printing
+print("{} + {} = {}".format(num1, num2, total))
